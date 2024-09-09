6000cd6f2216bb413c2bc314b224665e9cb2f458
Leo Siu <leonardoy.siu@gmail.com>
2024-09-08 18:32:39 -0700

    implement navbar

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import React from 'react';
+import { usePathname } from 'next/navigation';
+import { navLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button"
+
+
+const Navbar = () => {
+    const pathname = usePathname();
+
+    return (
+        <div className="flex size-full flex-row justify-between gap-4 p-8">
+            <Link href='/' className='navbar-logo'>
+                <Image src='/assets/icons/logo.png' alt='logo' width={100} height={100} />
+                <p className="navTitle">UC Irvine</p>
+            </Link>
+
+            <nav className="navbar-nav">
+                <ul className='navbar-nav-elements'>
+                    {navLinks.map((link) => {
+                        const isActive = link.route === pathname;
+
+                        return (
+                            <li key={link.route} className={`navbar-nav-element group ${
+                                isActive ? 'navLink-bold' : 'navLink'
+                            }`}>
+                                <Link className='' href={link.route}>
+                                    {link.label}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+
+            <Button asChild variant="outline" className='navbar-join-button hover:navbar-join-hover'>
+                <Link href="/join">Join Now</Link>
+            </Button>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ constants/index.ts
@@ -0,0 +1,101 @@
+export const navLinks = [
+    // {
+    //   label: "UC URVINE",
+    //   route: "/",
+    //   icon: "/assets/icons/logo.png",
+    // },
+    {
+      label: "About",
+      route: "/about",
+    //   icon: "/assets/icons/image.svg",
+    },
+    {
+      label: "Projects",
+      route: "/projects",
+    //   icon: "/assets/icons/stars.svg",
+    },
+    {
+      label: "Sponsors",
+      route: "/sponsors",
+    //   icon: "/assets/icons/scan.svg",
+    },
+    {
+      label: "Updates",
+      route: "/updates",
+    //   icon: "/assets/icons/filter.svg",
+    },
+    {
+      label: "Contact",
+      route: "/contact",
+    //   icon: "/assets/icons/camera.svg",
+    },
+    // {
+    //   label: "Join Now",
+    //   route: "/join",
+    // //   icon: "/assets/icons/profile.svg",
+    // },
+];
+  
+export const aboutLinks = [
+    {
+        label: "About Us",
+        route: "/about",
+        //   icon: "/assets/icons/logo.png",
+    },
+    {
+        label: "The Team",
+        route: "/about/team",
+        //   icon: "/assets/icons/image.svg",
+    },
+];
+  
+export const projectsLinks = [
+    {
+        label: "2024 - 2025",
+        route: "/projects/2025",
+        //   icon: "/assets/icons/logo.png",
+    },
+    {
+        label: "2023 - 2024",
+        route: "/projects/2024",
+        //   icon: "/assets/icons/logo.png",
+    },
+    {
+        label: "2022 - 2023",
+        route: "/projects/2023",
+        //   icon: "/assets/icons/logo.png",
+    },
+    {
+        label: "2021 - 2022",
+        route: "/projects/2022",
+        //   icon: "/assets/icons/logo.png",
+    },
+    {
+        label: "2020 - 2021",
+        route: "/projects/2021",
+        //   icon: "/assets/icons/logo.png",
+    },
+    // add more if needed
+];
+
+export const sponsorsLinks = [
+    {
+        label: 'Why Sponsor Us',
+        route: '/sponsors/why-sponsor-us',
+    },
+    {
+        label: 'Sponsorship Tiers',
+        route: '/sponsors/sponsorship-tiers',
+    },
+];
+
+export const updateLinks = [
+    {
+        label: 'Featured News',
+        route: '/updates/featured-news',
+    },
+    {
+        label: 'News Archive',
+        route: '/updates/news-archive',
+    }
+]

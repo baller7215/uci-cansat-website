ee7c9f31a9183940baeda97994d2b3bcd9c6133b
Leo Siu <leonardoy.siu@gmail.com>
2024-11-06 17:18:56 -0800

    minor changes to navbar to ensure it is more readable

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     const pathname = usePathname();
 
     return (
-        <div className="navbar">
+        <div className="navbar bg-gradient-to-b from-white/70 to-transparent">
             <Link href='/' className='navbar-logo'>
                 <Image src='/assets/icons/logo.png' alt='logo' width={75} height={75} />
                 <p className="navTitle">UC Irvine</p>

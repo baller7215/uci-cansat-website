bfe1b5231997f5b159c9629365851a3cea25a955
Leo Siu <leonardoy.siu@gmail.com>
2024-11-20 11:07:55 -0800

    fix responsiveness issue with contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,8 +11,8 @@ const Contact = async () => {
                 <CoverPage page="contact" title="Contact Us" background="../assets/images/competiton-photos/launch3.jpg" />
 
                 {/* contact form */}
-                <section className="section-container !bg-custom-lightBlack">
-                    <div className="text-container !flex-col text-center !justify-center p-10 md:!p-60">
+                <section className="section-container !h-auto !bg-custom-lightBlack">
+                    <div className="text-container !h-auto !flex-col text-center !justify-center p-10 md:!p-60 py-5 md:!py-20" id="overview">
                         <h2 className="header !m-0">We are excited to hear from you!</h2>
                         <p className="description text-custom-white">Please don’t hesitate to leave a message with any questions, concerns, or feedback you have!
                             <br></br>*If this message concerns sponsorship for UCI CanSat, please input your company&apos;s website and name, if applicable.</p>

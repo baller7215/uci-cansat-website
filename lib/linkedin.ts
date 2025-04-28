ee2f8d411bddf5de993e0619dc8da60906cfa7b5
Leo <leonardoy.siu@gmail.com>
2025-04-27 23:15:39 -0700

    remove base_url on client to just call relative path

--- lib/linkedin.ts
+++ lib/linkedin.ts
@@ -1,9 +1,9 @@
 import { LinkedInPost } from "@/components/shared/LinkedInCard";
 
 export async function fetchLinkedInFeed(): Promise<{ posts: LinkedInPost[] }> {
-    const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+    // const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-    const res = await fetch(`${BASE_URL}/api/linkedin-feed`);
+    const res = await fetch(`/api/linkedin-feed`);
     if (!res.ok) throw new Error('Network response was not ok');
     return res.json();
 }

8139668ec8efd6cbc5ab7e82ef31187791f28ec2
Leo Siu <leonardoy.siu@gmail.com>
2026-02-24 22:00:19 -0800

    refactor: modify sponsors page to dynamically pull sponsors from sanity

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,5 +1,6 @@
 import { sanityClient } from '@/lib/sanity.client' // adjust path if needed
 
+// FETCH TEAM MEMBERS BY YEAR
 export async function fetchTeamByYear(year: string) {
   const query = `*[_type == "member" && academicYear == $year]
     | order(order asc, name asc) {
@@ -18,6 +19,23 @@ export async function fetchTeamByYear(year: string) {
       order,
       personId,
       profileImg
+    }`
+  return sanityClient.fetch(query, { year }, { cache: "no-store" });
+}
+
+// FETCH SPONSORS BY YEAR
+export async function fetchSponsorsByYear(year: string) {
+  const query = `*[_type == "sponsor" && academicYear == $year]
+    | order(order desc, order desc) {
+      _id,
+      name,
+      website,
+      logo,
+      academicYear,
+      tier,
+      description,
+      order,
+      isActive,
     }`
   return sanityClient.fetch(query, { year }, { cache: "no-store" });
 }
